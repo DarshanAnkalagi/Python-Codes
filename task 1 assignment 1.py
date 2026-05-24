fd7afab42479433709b139bc3b58ef5846a069b5
Darshan Ankalagi <darshanankalagi7722@gmail.com>
2026-05-24 15:01:53 +0530

    Add files via upload
    This program perform simple mathematical operations on 2 numbers

---
+++ task 1 assignment 1.py
@@ -0,0 +1,10 @@
+n1=int(input("enter the first number: "))
+n2=int(input("enter the second number: "))
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+print("addition: ",add)
+print("substraction: ",sub)
+print("multiplication: ",mul)
+print("division: ",div)
