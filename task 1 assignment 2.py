0a77d03314037caf32d43f988b97fa5c73809406
Darshan Ankalagi <darshanankalagi7722@gmail.com>
2026-05-27 22:56:44 +0530

    Add files via upload

---
+++ task 1 assignment 2.py
@@ -0,0 +1,7 @@
+#program of check whether a number is even or odd
+number=int(input("enter a number: "))
+if number%2==0:
+    print(f"{number} is even.")
+else :
+    print(f"{number} is odd.")
+    
